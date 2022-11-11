97c22190ae88483c4a0e40612db6652d79c98016
erickGzz <eagm080897@gmail.com>
2022-11-10 22:57:04 -0600

    add basic frontend setup with react

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>Header</h1>
+        <nav>
+          <a href="">1</a>
+          <a href="">2</a>
+          <a href="">3</a>
+        </nav>
+      </header>
+
+      <main>
+        <h2>main content</h2>
+      </main>
+
+      <footer>
+        <h2>footer content</h2>
+      </footer>
+    </>
+  );
+}
+
+export default App;
